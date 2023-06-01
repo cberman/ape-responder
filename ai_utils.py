561368cb47d19cff7a4d5f3f1d763f688fcb819b
Collin Berman <collinberman1@gmail.com>
2023-06-01 12:23:17 -0700

    make the ape and chat personalities more distinct and better

--- ai_utils.py
+++ ai_utils.py
@@ -40,17 +40,21 @@ that is all: only output the json without extra information outside the json obj
             template_format='jinja2'
             )
 
-ape_context = 'We have a discord full of individuals with below average intelligence. They often neglect to respond to pings in a timely manner. They have a helpful gorilla assistant who can make a best effort to respond for users when they miss a ping. The gorilla is also busy so responses are fraught with inaccuracies and errors, on top of the already uninteligble gorilla noises. Despite all that, the gorilla tries to answer in the style of the user.'
-chat_inputs = {'ping': 'the ping that the user missed',
+ape_context = 'We have a discord full of individuals with below average intelligence. They often neglect to respond to pings in a timely manner. They have a helpful gorilla assistant who can make a best effort to respond for users when they miss a ping. The gorilla is also busy so responses are fraught with inaccuracies and errors, on top of the already uninteligble gorilla noises. Despite all that, the gorilla tries to answer in the style of the user that it is replying for.'
+chat_inputs = {'username': 'the display name of the user that missed the ping',
+               'ping': 'the ping that "{username}" missed',
                'sample_chats': 'a list of previous chats by the user. they probably do not provide anything relevant to the ping. but they are useful for emulating the style of the user.',
                # 'sample_pings': 'a list of previous responses by the user after they are pinged',
+    }
+ape_inputs = {'ping': 'a dm from a user to the gorilla',
+              'sample_chats': 'a list of previous chats by the user and the gorilla. they be relevant to the ping, but most are not.',
     }
 chat_output_field = 'response'
 ape_output_desc = 'the gorilla assistant\'s response to the user\'s ping. make sure to include a response of some sort, even if the gorilla has nothing to say. please answer as though you were a gorilla who was semi-capable of speaking english and was also famished for bananas at the time of responding.'
 chat_output_desc = 'the gorilla assistant\'s response, for what it thinks the user would say to the ping. make sure to include a response of some sort, even if the gorilla has nothing to say. remember, you are trying to answer in the style of the user.'
 ape_think = 'like a gorilla'
-chat_think = 'like the user'
-ape_template = build_prompt_template(ape_context, chat_inputs, chat_output_field, ape_output_desc, think=ape_think)
+chat_think = 'like {username}'
+ape_template = build_prompt_template(ape_context, ape_inputs, chat_output_field, ape_output_desc, think=ape_think)
 chat_template = build_prompt_template(ape_context, chat_inputs, chat_output_field, chat_output_desc, think=chat_think)
 
 def heal_response(raw_response, prefix=response_template_starter):
@@ -79,7 +83,7 @@ def heal_response(raw_response, prefix=response_template_starter):
                     pass
     return raw_response
 
-def get_ape_response(openai, ping, sample_chats, sample_pings=''):
+def get_ape_response(openai, ping, sample_chats):
     raw_response = openai(
             ape_template.format(
                     ping=ping,
@@ -89,12 +93,13 @@ def get_ape_response(openai, ping, sample_chats, sample_pings=''):
     prefix = f'{response_template_starter} {ape_think}. '
     return heal_response(raw_response, prefix)
 
-def get_chat_response(openai, ping, sample_chats, sample_pings=''):
+def get_chat_response(openai, username, ping, sample_chats, sample_pings=''):
     raw_response = openai(
             chat_template.format(
+                    username=username,
                     ping=ping,
                     sample_chats=sample_chats
                     #sample_pings=sample_pings
                 ))
-    prefix = f'{response_template_starter} {chat_think}. '
+    prefix = f'{response_template_starter} the user. '
     return heal_response(raw_response, prefix)
--- bot.py
+++ bot.py
@@ -9,8 +9,8 @@ openai = OpenAI(
     openai_api_key=config.OPENAI_API_KEY,
 )
 
-def get_ai_response(f, pingee, ping, chat_history):
-    raw_ai_response = f(openai, ping, chat_history, '')
+def get_ai_response(f, kwargs):
+    raw_ai_response = f(openai, **kwargs)
     try:
         parsed_ai_response = json.loads(raw_ai_response)
         print(json.dumps(parsed_ai_response, indent=2))
@@ -73,17 +73,19 @@ async def on_message(message):
 
     for pingee in message.mentions:
         if pingee == client.user:
+            # ape was pinged directly
             print(message.content)
             print('responding as Gorilla')
-            if message.author in config.VERIFIED_USERS:
+            ape_history = get_user_history(client.user)
+            history_string = '\n'.join(ape_history)
+            if message.author.display_name in config.VERIFIED_USERS:
                 user_history = get_user_history(message.author)
-                history_string = '\n'.join(user_history)
-            else:
-                history_string = '\n'
+                history_string += '\n' + '\n'.join(user_history)
             loop = asyncio.get_event_loop()  # Get the current event loop
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 # Run the get_ape_response function in the executor
-                future = loop.run_in_executor(executor, get_ai_response, ai_utils.get_ape_response, pingee, message.content, history_string)
+                future = loop.run_in_executor(executor, get_ai_response, ai_utils.get_ape_response, {'ping':message.content, 'sample_chats':history_string})
+
                 ape_response = await future  # This will complete once get_ape_response has completed
             await message.reply(ape_response)
 
@@ -100,7 +102,7 @@ async def on_message(message):
             loop = asyncio.get_event_loop()  # Get the current event loop
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 # Run the get_chat_response function in the executor
-                future = loop.run_in_executor(executor, get_ai_response, ai_utils.get_chat_response, pingee, message.content, history_string)
+                future = loop.run_in_executor(executor, get_ai_response, ai_utils.get_chat_response, {'username':pingee, 'ping':message.content, 'sample_chats':history_string})
                 chat_response = await future  # This will complete once get_ape_response has completed
             await message.reply(f'{pingee.display_name}: {chat_response}')
 
